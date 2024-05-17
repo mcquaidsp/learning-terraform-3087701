fe3b4d6ba1c39e01942e55e54b77c69ab72afda1
mcquaidsp <160052688+mcquaidsp@users.noreply.github.com>
2024-05-17 10:37:03 +0100

    load balancer may 17

--- main.tf
+++ main.tf
@@ -56,29 +56,60 @@ module "alb" {
   subnets = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
 
-http_tcp_listeners = [
-    {
-      port = 80
+  # Security Group
+  security_group_ingress_rules = {
+    all_http = {
+      from_port   = 80
+      to_port     = 80
+      ip_protocol = "tcp"
+      description = "HTTP web traffic"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+    all_https = {
+      from_port   = 443
+      to_port     = 443
+      ip_protocol = "tcp"
+      description = "HTTPS web traffic"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+  }
+  security_group_egress_rules = {
+    all = {
+      ip_protocol = "-1"
+      cidr_ipv4   = "10.0.0.0/16"
+    }
+  }
+
+  
+  listeners = {
+    ex-http-https-redirect = {
+      port     = 80
       protocol = "HTTP"
       target_group_index = 0
     }
-  ]
+    
+  }
 
   target_groups = {
     ex-instance = {
-      name_prefix      = "blog"
+      name_prefix      = "blog-"
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
       targets = {
-        my_target = {
-            target_id = aws_instance.blog.id
-            port = 80
-        }
+          my_target = {
+              target_id = aws_instance.blog.id
+              port = 80
+          }
       }
     }
   }
+
+  tags = {
+    Environment = "Development"
+    Project     = "Example"
   }
+}
   
 
 module "blog_sg" {
