6f092a9ba4879c5251fa323d14417c4bd2e3ad4c
mcquaidsp <160052688+mcquaidsp@users.noreply.github.com>
2024-05-17 13:35:19 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "instance_ami" {
-  value = aws_instance.blog3.ami
+  value = aws_instance.blog.ami
 }
 
 output "instance_arn" {
-  value = aws_instance.blog3.arn
+  value = aws_instance.blog.arn
 }
